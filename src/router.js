4b11ecbfdecddce776b7391fa63d40b22b83d0c8
mike <boimik87@gmail.com>
2018-12-05 15:31:00 +0000

    create router

---
+++ src/router.js
@@ -0,0 +1,19 @@
+const handler = require("./handler.js");
+
+const routes = ["/style.css", "/main.js", "/index.html"];
+
+const router = (req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    console.log("home is home");
+    // handler.home;
+  } else if (routes.includes(url)) {
+    console.log("./public/index.html");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("404, file not found!");
+    console.log("404");
+  }
+};
+
+module.exports = router;
