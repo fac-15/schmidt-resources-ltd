382c4ec1203476ef512b4b9e577f048e8d62398c
mike <boimik87@gmail.com>
2018-12-06 12:56:09 +0000

    add tables type skill level

--- database/db_build.sql
+++ database/db_build.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+-- resource table
 DROP TABLE IF EXISTS resources CASCADE;
 CREATE TABLE resources (
   id SERIAL PRIMARY KEY,
@@ -15,7 +16,53 @@ INSERT INTO resources (name, type, skill, level, location) VALUES
 ('online css', 'online', 'css','hard', 'www.css.com'),
 ('book html', 'book', 'html','medium', 'fac'),
 ('harry', 'human', 'html','easy', 'pub'),
-('css', 'book', 'css','easy', 'fac');
+('css', 'book', 'css','easy', 'fac'),
+('NodeJs', 'book', 'css','easy', 'fac');
 
+-- type table
+
+DROP TABLE IF EXISTS type CASCADE;
+CREATE TABLE type (
+  name VARCHAR(10) NOT NULL,
+  index INTEGER NOT NULL
+);
+
+INSERT INTO type (name, index) VALUES
+('All', '1'),
+('Book', '2'),
+('Human', '3'),
+('Online', '4');
+
+-- skill table
+
+DROP TABLE IF EXISTS skill CASCADE;
+CREATE TABLE skill (
+  name VARCHAR(12) NOT NULL,
+  index INTEGER NOT NULL,
+  level VARCHAR(12) 
+);
+
+INSERT INTO skill (name, index, level) VALUES
+('All', '1'),
+('CSS', '2', 'beginner'),
+('JavaScript', '3', 'medium'),
+('HTML', '4', 'beginner'),
+('NodeJS', '5', 'advanced');
+
+-- level table
+
+DROP TABLE IF EXISTS level CASCADE;
+CREATE TABLE level (
+  name VARCHAR(10) NOT NULL,
+  index INTEGER NOT NULL
+);
+
+INSERT INTO type (name, index) VALUES
+('All', '1'),
+('Easy', '2'),
+('Medium', '3'),
+('Hard', '4');
 
 COMMIT;
+
+
